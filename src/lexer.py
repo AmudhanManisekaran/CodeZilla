0bd31bc1335bd679610656711bfd65116940e524
Kartik Mathpal <kmathpal684@gmail.com>
2020-04-24 22:58:17 -0700

    adds lexer.py to src

---
+++ src/lexer.py
@@ -0,0 +1,90 @@
+# lexer
+
+from sly import Lexer
+import pickle 
+import simplejson
+import os
+
+class CalcLexer(Lexer):
+    # Set of token names.   This is always required
+    tokens = { NUMBER, ID, WHILE, IF, ELSE, PRINT,
+               PLUS, MINUS, TIMES, DIVIDE, ASSIGN,
+               EQ, LT, LE, GT, GE, NE, FOR, ENDWHILE, ENDIF, SHOW, DO, END, LTA, GTA }
+
+
+    literals = { '(', ')', '{', '}', ';', ',', ':', '\'', ':=' }
+
+    # String containing ignored characters
+    ignore = ' \t'
+
+    # Regular expression rules for tokens
+    PLUS    = r'\+'
+    MINUS   = r'-'
+    TIMES   = r'\*'
+    DIVIDE  = r'/'
+    EQ      = r'=='
+    ASSIGN  = r'='
+    LE      = r'<='
+    LT      = r'<'
+    GE      = r'>='
+    GT      = r'>'
+    NE      = r'!='
+ 
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    # Identifiers and keywords
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+    ID['print'] = PRINT
+    ID['for'] = FOR
+    ID['endwhile'] = ENDWHILE
+    ID['endif'] = ENDIF
+    ID['show'] = SHOW
+    ID['do'] = DO 
+    ID['End'] = END
+    ID['<<'] = LTA
+    ID['>>'] = GTA
+
+    ignore_comment = r'\#.*'
+
+    # Line number tracking
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
+
+if __name__ == '__main__':
+
+    print("\n****************Start Execution****************")
+    inputFile = input('Lexer > ')
+    str = open(inputFile, 'r').read()
+    arr = []
+    lexer = CalcLexer()
+    for tok in lexer.tokenize(str):
+       arr.append(tok.value)     
+    f = open('output.tok','w')
+    simplejson.dump(arr,f)
+    f.close()
+         
+    f = open('output.tok','r')
+    arr =  []
+    result = []
+    arr = f.read()
+    str1=""
+    for x in arr:
+        x = x.replace('"','')
+        str1+=x  
+    file = open('result.tok','w')
+    file.write(str1)
+    file.close()
+    print("\nresult.tok generated!!!")
+    print("\n****************End Of Execution****************")
